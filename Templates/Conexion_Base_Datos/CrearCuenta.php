d37e2726e6e3b255d84d21acd840d06f2780dcbd
Alejandro Alonso <programadoralejandro371@gmail.com>
2023-11-15 11:39:28 -0600

    Cambios de inicio de sesion y crear cuenta

--- Templates/Conexion_Base_Datos/CrearCuenta.php
+++ Templates/Conexion_Base_Datos/CrearCuenta.php
@@ -12,7 +12,15 @@
 
       if ($resultado->num_rows > 0) {
           // Si las credenciales son válidas, inicia sesión o realiza alguna acción requerida
-          echo json_encode(["mensaje" => "Inicio de sesión exitoso"]);
+          echo json_encode(["mensaje" => "Ya existe este suario"]);
+      } else {
+          // Si las credenciales no son válidas, guarda los datos en la base de datos
+          $insertar = "INSERT INTO Cliente (correo, contraseña) VALUES ('$correo', '$contrasena')";
+          if ($conexion->query($insertar) === TRUE) {
+              echo json_encode(["mensaje" => "Datos guardados en la base de datos"]);
+          } else {
+              echo json_encode(["mensaje" => "Error al guardar datos: " . $conexion->error]);
+          }
       }
   } else {
       echo json_encode(["mensaje" => "Método no permitido"]);
