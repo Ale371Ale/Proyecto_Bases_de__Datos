e46a272c55ae7f7e447f799397c56534d473b021
Cesar Donnet Hernandez Gastelum <al283189@edu.uaa.mx>
2023-11-22 16:21:15 -0600

    lee y muestra los productos

--- Templates/Pagina_Nueva/MisProductos.js
+++ Templates/Pagina_Nueva/MisProductos.js
@@ -113,117 +113,134 @@ console.error('Error al obtener el ID:', error);
 
 
 
+var productosVendedor = [];
+
 
 async function LeerProductos() {
-try {
-await obtenerId(); // Esperar a que obtenerId se complete antes de continuar
-// Realizar la solicitud fetch para obtener productos
-const response = await fetch('LeerProductos.php', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ idVendedor: window.idVendedor }),
-});
-
-if (!response.ok) {
-    throw new Error('Respuesta no exitosa: ' + response.statusText);
-}
-
-const productos = await response.json();
-// Limpiar el contenedor donde se mostrarán los productos
-$('#listaProductos').empty();
-
-// Iterar sobre los productos y mostrarlos
-for (var i = 0; i < productos.length; i++) {
-    var producto = productos[i];
-
-    // Crear un elemento para mostrar el producto
-    var elementoProducto = $('<div></div>').html(
-        '<strong>' + producto.Nombre + '</strong> - ' +
-        'Descripción: ' + producto.Descripcion + ', ' +
-        'Precio: ' + producto.precio + ', ' +
-        'Vistas: ' + producto.Vistas +
-        ' Enlace: <a href="' + producto.DireccionWeb + '" target="_blank">Ver producto</a>'
-        // Puedes agregar más campos según tus necesidades
-    );
-
-    // Agregar la imagen si está presente
-    if (producto.Imagen) {
-        var imagenElemento = $('<img>').attr('src', 'data:image/png;base64,' + producto.Imagen);
-        elementoProducto.append(imagenElemento);
-    }
-
-    // Agregar el elemento del producto al contenedor
-    $('#listaProductos').append(elementoProducto);
-
-
-}
-} catch (error) {
-console.error('Error al obtener productos:', error);
-}
-}
-
-
-
-var productosVendedor = [];
-function mostrarProductos() {
-    LeerProductos();
-var listaProductos = document.getElementById('listaProductos');
-listaProductos.innerHTML = '';
-
-// Crear un contenedor principal para las columnas
-var contenedorColumnas = document.createElement('div');
-contenedorColumnas.className = 'row';
-
-productosVendedor.forEach(function (producto) {
-// Crear un elemento para cada tarjeta de producto
-var elementoProducto = document.createElement('div');
-elementoProducto.className = 'col-md-4';
-elementoProducto.innerHTML = `
-    <div class="card">
-        <div class="card-body">
-            <h5 class="card-title">${producto.nombre}</h5>
-            <p class="card-text" style="max-lines: 3;">Precio: $${producto.precio}</p>
-            <!-- Agrega más información según sea necesario -->
-            <a href="${producto.enlace}" class="btn btn-primary">Ver Artículo</a>
-            <a onclick="addToCart('${producto.imagen}', '${producto.nombre}', ${producto.precio})" class="btn btn-primary">Agregar a Carrito</a>
-        </div>
-        <img src="${producto.imagen}" class="card-img-top" style="height: 50px; width: 50px; cursor: pointer;" alt="${producto.nombre}" onclick="window.location.href='${producto.enlace}'">
-    </div>
-`;
-
-// Agregar el elemento de la tarjeta al contenedor de columnas
-contenedorColumnas.appendChild(elementoProducto);
-});
-
-// Agregar el contenedor de columnas al contenedor principal
-listaProductos.appendChild(contenedorColumnas);
-}
-
-function filtrarProductos(event) {
-    var categoriaSeleccionada = event.target.getAttribute('data-categoria');
-
-    var productosFiltrados = [];
-
-    if (categoriaSeleccionada === 'todos') {
-        productosFiltrados = productosVendedor;
-    } else {
-        productosFiltrados = productosVendedor.filter(function (producto) {
-            return producto.categoria === categoriaSeleccionada;
+    try {
+        await obtenerId();
+        const response = await fetch('LeerProductos.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ idVendedor: window.idVendedor }),
         });
-    }
 
-    mostrarProductos(productosFiltrados);
+        if (!response.ok) {
+            throw new Error('Respuesta no exitosa: ' + response.statusText);
+        }
+
+        const productos = await response.json();
+        productosVendedor = productos.map(producto => ({
+            nombre: producto.Nombre,
+            descripcion: producto.Descripcion,
+            precio: producto.precio,
+            vistas: producto.Vistas,
+            enlace: producto.DireccionWeb,
+            imagen: producto.Imagen ? 'data:image/png;base64,' + producto.Imagen : null,
+        }));
+
+    } catch (error) {
+        console.error('Error al obtener productos:', error);
+    }
 }
 
-mostrarProductos();
 
-document.addEventListener('DOMContentLoaded', function () {
-    var nuevosProductosLink = document.getElementById('nuevosProductosLink');
-    nuevosProductosLink.addEventListener('click', function () {
-        var addProductModal = new bootstrap.Modal(document.getElementById('addProductModal'));
-        addProductModal.show();
+function mostrarProductos() {
+    try {
+        LeerProductos().then(() => {
+            var listaProductos = document.getElementById('listaProductos');
+            listaProductos.innerHTML = '';
+
+            // Crear un contenedor principal para las columnas
+            var contenedorColumnas = document.createElement('div');
+            contenedorColumnas.className = 'row';
+
+            productosVendedor.forEach(producto => {
+                // Crear un elemento para cada tarjeta de producto
+                var elementoProducto = document.createElement('div');
+                elementoProducto.className = 'col-md-4 mb-4'; // Añadir clase mb-4 para agregar espacio entre las tarjetas
+
+                var card = document.createElement('div');
+                card.className = 'card h-100';
+
+                var img = document.createElement('img');
+                img.src = producto.imagen;
+                img.className = 'card-img-top';
+                img.style = 'height: 200px; object-fit: contain; cursor: pointer;';
+                img.alt = producto.nombre;
+                img.onclick = function () {
+                    window.location.href = producto.enlace;
+                };
+
+                var cardBody = document.createElement('div');
+                cardBody.className = 'card-body';
+
+                var title = document.createElement('h5');
+                title.className = 'card-title';
+                title.textContent = producto.nombre;
+
+                var price = document.createElement('p');
+                price.className = 'card-text';
+                price.textContent = 'Precio: $' + producto.precio;
+
+                var viewButton = document.createElement('a');
+                viewButton.href = producto.enlace;
+                viewButton.className = 'btn btn-primary btn-sm';
+                viewButton.textContent = 'Ver Artículo';
+
+                var addButton = document.createElement('a');
+                addButton.onclick = function () {
+                    addToCart(producto.imagen, producto.nombre, producto.precio);
+                };
+                addButton.className = 'btn btn-primary btn-sm';
+                addButton.textContent = 'Agregar a Carrito';
+
+                cardBody.appendChild(title);
+                cardBody.appendChild(price);
+                cardBody.appendChild(viewButton);
+                cardBody.appendChild(addButton);
+
+                card.appendChild(img);
+                card.appendChild(cardBody);
+
+                elementoProducto.appendChild(card);
+
+                // Agregar el elemento de la tarjeta al contenedor de columnas
+                contenedorColumnas.appendChild(elementoProducto);
+            });
+
+            listaProductos.appendChild(contenedorColumnas);
+        });
+    } catch (error) {
+        console.error('Error al mostrar productos:', error);
+    }
+}
+
+    function filtrarProductos(event) {
+        var categoriaSeleccionada = event.target.getAttribute('data-categoria');
+
+        var productosFiltrados = [];
+
+        if (categoriaSeleccionada === 'todos') {
+            productosFiltrados = productosVendedor;
+        } else {
+            productosFiltrados = productosVendedor.filter(function (producto) {
+                return producto.categoria === categoriaSeleccionada;
+            });
+        }
+
+        mostrarProductos(productosFiltrados);
+    }
+
+    mostrarProductos();
+
+    document.addEventListener('DOMContentLoaded', function () {
+        var nuevosProductosLink = document.getElementById('nuevosProductosLink');
+        nuevosProductosLink.addEventListener('click', function () {
+            var addProductModal = new bootstrap.Modal(document.getElementById('addProductModal'));
+            addProductModal.show();
+        });
     });
-});
-    // Llamar a la función al cargar la página (puedes ajustar esto según tus necesidades)
+        // Llamar a la función al cargar la página (puedes ajustar esto según tus necesidades)
